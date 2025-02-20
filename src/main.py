a6d92164004769a26ef773815f68e9e2dbc40626
AlbertSabirzianov <albertuno@mail.ru>
2025-02-20 11:18:51 +0300

    Добавил поиск картинки из яндекса

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from telegram import Bot
 from app.settings import SbSettings, TgSettings
 from app.enums import ChatMessage, ChatContext
 from gigachat.chat import get_giga_chat_answer
+from yandex.pictures import get_picture
 
 
 def parse_titles(response: str) -> list[str]:
@@ -26,29 +27,32 @@ def get_list_of_titles(authorization_sb_code: str) -> list[str]:
     return parse_titles(response)
 
 
-def send_to_channels(channels: list[str], message: str, bot_token: str) -> None:
+def send_to_channels(channels: list[str], message: str, bot_token: str, picture: bytes) -> None:
     for ch in channels:
         bot = Bot(token=bot_token)
-        asyncio.run(bot.send_message(chat_id=ch, text=message))
+        asyncio.run(bot.send_photo(chat_id=ch, caption=message, photo=picture))
 
 
 def main():
     tg_settings = TgSettings()
     sb_settings = SbSettings()
 
-    # print(tg_settings.chanel_names)
     titles = get_list_of_titles(sb_settings.authorization_sb_code)
-
     today_title = random.choice(titles)
+    print(f"Тема {today_title}")
     today_post = get_giga_chat_answer(
         message=today_title,
         context=ChatContext.GET_POST_CONTEXT.value,
         authorization_sb_code=sb_settings.authorization_sb_code
     )
+    print(today_post)
+
+    picture = get_picture(today_title)
     send_to_channels(
         channels=tg_settings.chanel_names,
         message=today_post,
-        bot_token=tg_settings.bot_token
+        bot_token=tg_settings.bot_token,
+        picture=picture
     )
 
 
