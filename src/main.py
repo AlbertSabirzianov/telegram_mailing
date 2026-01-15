adee33559dc7f07153651ffb9239148b8f01af5d
AlbertSabirzianov <albertuno@mail.ru>
2026-01-15 11:46:26 +0300

    Добавил ввод темы поста от пользователя и убрал лишнее

--- src/main.py
+++ src/main.py
@@ -1,45 +1,30 @@
 import asyncio
-import random
 
 from dotenv import load_dotenv
 from telegram import Bot
 
 from app.settings import SbSettings, TgSettings
-from app.enums import ChatMessage, ChatContext
+from app.enums import ChatContext
 from gigachat.chat import get_giga_chat_answer
 from yandex.pictures import get_picture
 
-
-def parse_titles(response: str) -> list[str]:
-    # Разбиваем текст по строкам
-    lines = response.strip().split('\n')
-    # Ищем строки, которые начинаются с цифры и точки
-    ideas = [line.strip().split('.')[1] for line in lines if line.strip().startswith(tuple(f"{i}." for i in range(1, 11)))]
-    return ideas
-
-
-def get_list_of_titles(authorization_sb_code: str) -> list[str]:
-    response: str = get_giga_chat_answer(
-        message=ChatMessage.GET_TITLES_QUERY.value,
-        context=ChatContext.GET_TITLES_CONTEXT.value,
-        authorization_sb_code=authorization_sb_code
-    )
-    return parse_titles(response)
-
-
 def send_to_channels(channels: list[str], message: str, bot_token: str, picture: bytes) -> None:
     for ch in channels:
-        bot = Bot(token=bot_token)
-        asyncio.run(bot.send_photo(chat_id=ch, caption=message, photo=picture))
+        while True:
+            try:
+                bot = Bot(token=bot_token)
+                asyncio.run(bot.send_photo(chat_id=ch, caption=message, photo=picture))
+                break
+            except:
+                print("Can't send to telegramm ....")
 
 
 def main():
     tg_settings = TgSettings()
     sb_settings = SbSettings()
 
-    titles = get_list_of_titles(sb_settings.authorization_sb_code)
+    today_title = input("\n\n Введите тему поста: ")
     while True:
-        today_title = random.choice(titles)
         print(f"Тема {today_title}")
         today_post = get_giga_chat_answer(
             message=today_title,
@@ -50,8 +35,11 @@ def main():
         if len(today_post) < 200 or len(today_post) > 1000:
             print("Post too small or too long")
             continue
-
-        picture = get_picture(today_title)
+        try:
+            picture = get_picture(today_title)
+        except Exception:
+            with open("img.png", "rb") as file:
+                picture = file.read()
         send_to_channels(
             channels=tg_settings.chanel_names,
             message=today_post,
@@ -64,6 +52,5 @@ def main():
 if __name__ == "__main__":
     load_dotenv()
     main()
-
 
 
--- src/yandex/pictures.py
+++ src/yandex/pictures.py
@@ -1,13 +1,14 @@
+import random
+
 import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium_stealth import stealth
 from webdriver_manager.chrome import ChromeDriverManager
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 Y_URL = "https://yandex.ru/images/search?from=tabbar&text=<q_text>"
 
@@ -33,7 +34,8 @@ class SDriver:
             fix_hairline=True
         )
         self.driver.execute_cdp_cmd('Network.setUserAgentOverride', {
-            'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                         ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         })
         return self.driver
 
@@ -44,10 +46,10 @@ class SDriver:
 def get_picture(q: str) -> bytes:
     with SDriver() as driver:
         driver.get(Y_URL.replace("<q_text>", q))
-        img_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "img.ImagesContentImage-Image"))
+        img_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "img.ImagesContentImage-Image"))
         )
-        img_url = img_element.get_attribute("src")
+        img_url = random.choice(img_elements).get_attribute("src")
     response = requests.get(img_url)
     return response.content
 
